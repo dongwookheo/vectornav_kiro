156d1b893c7f7f90a025affce3ef0039550fbd22
dongwookheo <hdwook3918@gmail.com>
2026-06-09 12:09:22 +0900

    Tune VN100 configuration

--- vectornav/launch/vectornav.launch.py
+++ vectornav/launch/vectornav.launch.py
@@ -5,22 +5,26 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
 
-    this_dir = get_package_share_directory('vectornav')
-    
+    this_dir = get_package_share_directory("vectornav")
+    config_path = os.path.join(this_dir, "config", "vn_100_800hz.yaml")
+
     # Vectornav
     start_vectornav_cmd = Node(
-        package='vectornav', 
-        executable='vectornav',
-        output='screen',
-        parameters=[os.path.join(this_dir, 'config', 'vectornav.yaml')])
-    
+        package="vectornav",
+        executable="vectornav",
+        output="screen",
+        parameters=[config_path],
+    )
+
     start_vectornav_sensor_msgs_cmd = Node(
-        package='vectornav', 
-        executable='vn_sensor_msgs',
-        output='screen',
-        parameters=[os.path.join(this_dir, 'config', 'vectornav.yaml')])
+        package="vectornav",
+        executable="vn_sensor_msgs",
+        output="screen",
+        parameters=[config_path],
+    )
 
     # Create the launch description and populate
     ld = LaunchDescription()
